741254b4ee7a4fd89521d0fc4681010d1532cace
djcarrad <djcarrad@gmail.com>
2025-02-05 19:49:11 +0100

    introdude manual_inflection into examples notebook

--- mobilityapp/__init__.py
+++ mobilityapp/__init__.py
@@ -1,6 +1,10 @@
 from .app import run
 
-from .supportfunctions import perform_deriv_fit, perform_drude_fit, perform_Rs_fit, perform_entire_prodecure
+from .supportfunctions import (perform_deriv_fit, 
+                                perform_drude_fit, 
+                                perform_Rs_fit, 
+                                perform_entire_prodecure,
+                                manual_inflection)
 
 def main():
     run()
